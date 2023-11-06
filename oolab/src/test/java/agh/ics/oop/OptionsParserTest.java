6cc1aea11cf23c9ff8e4c64ca57ccb79400597e6
Jakub Worek <worekkuba@gmail.com>
2023-11-06 16:06:14 +0100

    OptionsParserTest moved

--- oolab/src/test/java/agh/ics/oop/OptionsParserTest.java
+++ oolab/src/test/java/agh/ics/oop/OptionsParserTest.java
@@ -1,5 +1,6 @@
-package agh.ics.oop.model;
-import agh.ics.oop.OptionParser;
+package agh.ics.oop;
+
+import agh.ics.oop.model.MoveDirection;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
