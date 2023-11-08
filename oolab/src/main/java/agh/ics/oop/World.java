2081187337eec5c0c41a4afd51921712a32811dd
Jakub Worek <worekkuba@gmail.com>
2023-11-08 12:44:00 +0100

    lab4 extended task TextMap with tests

--- oolab/src/main/java/agh/ics/oop/World.java
+++ oolab/src/main/java/agh/ics/oop/World.java
@@ -1,8 +1,5 @@
 package agh.ics.oop;
-import agh.ics.oop.model.MoveDirection;
-import agh.ics.oop.model.RectangularMap;
-import agh.ics.oop.model.Vector2d;
-import agh.ics.oop.model.WorldMap;
+import agh.ics.oop.model.*;
 
 import java.util.List;
 import static agh.ics.oop.OptionParser.parse;
@@ -12,11 +9,22 @@ public class World {
     public static void main(String[] args){
         System.out.println("system wystartowal");
 
+        // RectangularMap
         List<MoveDirection> directions = parse(args);
         List<Vector2d> positions = List.of(new Vector2d(2,2), new Vector2d(3,4));
-        WorldMap map = new RectangularMap(5,5);
+        WorldMap<Animal, Vector2d> map = new RectangularMap(5,5);
         Simulation simulation = new Simulation(directions, positions, map);
         simulation.run();
+
+        // TextMap
+        List<String> texts = List.of("Ala", "ma", "sowoniedźwiedzia");
+        WorldMap<String, Integer> textMap = new TextMap();
+        for (String text : texts) {
+            textMap.place(text);
+        }
+        System.out.println(textMap);
+        textMap.move("ma", MoveDirection.FORWARD);
+        System.out.println(textMap);
 
         System.out.println("system zakonczyl dzialanie");
     }
