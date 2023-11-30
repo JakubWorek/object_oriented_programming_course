fc9c80c35fed48cfac591811181dea81bbf1160f
Jakub Worek <worekkuba@gmail.com>
2023-11-30 16:00:31 +0100

    animal tests

--- oolab/src/test/java/agh/ics/oop/model/AnimalTest.java
+++ oolab/src/test/java/agh/ics/oop/model/AnimalTest.java
@@ -10,6 +10,24 @@ public class AnimalTest {
     }
 
     @Test
+    void testToString2() {
+        Animal animal = new Animal(MapDirection.EAST, new Vector2d(2, 2));
+        assert animal.toString().equals("E");
+    }
+
+    @Test
+    void testToString3() {
+        Animal animal = new Animal(MapDirection.SOUTH, new Vector2d(2, 2));
+        assert animal.toString().equals("S");
+    }
+
+    @Test
+    void testToString4() {
+        Animal animal = new Animal(MapDirection.WEST, new Vector2d(2, 2));
+        assert animal.toString().equals("W");
+    }
+
+    @Test
     void testGetPosition() {
         Animal animal = new Animal();
         assert animal.getPosition().equals(new Vector2d(2, 2));
